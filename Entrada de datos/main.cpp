02b2c9a9d7a6e7f43e71631a1255abd4059c7d04
Eduardo Pineda Guerra <37886408+NeoMty@users.noreply.github.com>
2020-09-05 16:09:03 -0500

    Add data input

---
+++ Entrada de datos/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numero; //  numero entero
+    cout << "Escribe un numero: ";
+    cin >> numero;
+    cout << "El numero que escribiste es: " << numero << endl;
+
+    float numero2;  //  numero flotante
+    cout << "Escribe un numero: ";
+    cin >> numero2;
+    cout << "El numero que escribiste es: " << numero2 << endl;
+
+    return 0;
+}
